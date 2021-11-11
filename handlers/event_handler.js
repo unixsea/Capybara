26d3c95e3e52f55bd146627c87e69a2e759c0b0b
null <60427892+vierofernando@users.noreply.github.com>
2021-11-12 01:21:34 +0700

    perf(event_handler): use global variable & slice

--- handlers/event_handler.js
+++ handlers/event_handler.js
@@ -1,18 +1,16 @@
 const fs = require("fs");
+const dirNames = ["client", "guild"];
 
 // Starting out event handler
 module.exports = (client, Discord) => {
-  const load_dir = (dirs) => {
+  for (const dirs of dirNames) {
     const event_files = fs
       .readdirSync(`./events/${dirs}`)
       .filter((file) => file.endsWith(".js"));
 
     for (const file of event_files) {
       const event = require(`../events/${dirs}/${file}`);
-      const event_name = file.split(".")[0];
-      client.on(event_name, event.bind(null, Discord, client));
+      client.on(file.slice(0, 3), event.bind(null, Discord, client));
     }
-  };
-
-  ["client", "guild"].forEach((e) => load_dir(e));
+  }
 };
