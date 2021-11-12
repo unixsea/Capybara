99083fc46edef092ac3428e8c5bf32bb3e3c8c3e
null <60427892+vierofernando@users.noreply.github.com>
2021-11-12 09:42:21 +0700

    fix(messageCreate): okay, but i still win

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -2,24 +2,12 @@ const { MessageEmbed } = require("discord.js");
 const { inspect } = require("util");
 
 const prefix = "c+";
-const commandNotFoundEmbed = new MessageEmbed()
-  .setTitle(
-    "Homie looks like that doesn't command exists."
-  )
-  .setDescription(
-    `Looking for a command? I got you homie, just do c+about to see all my commands and see what I do!`
-  )
-  .setFooter(
-    `${message.author.tag}`,
-    message.author.displayAvatarURL({ dynamic: true })
-  )
-  .setTimestamp();
 
 // Starting our message event.
 module.exports = async (Discord, client, message) => {
   // If the mesasge doesn't start with prefix or if its the author / bot just return.
   if (message.author.bot || !message.content.startsWith(prefix)) return;
-
+  
   const args = message.content.slice(prefix.length).split(/\s+/);
   const cmd = args.shift().toLowerCase();
 
@@ -28,7 +16,23 @@ module.exports = async (Discord, client, message) => {
     client.commands.find((a) => a.aliases && a.aliases.includes(cmd));
   
   // If command doesn't exist, send an embed.
-  if (!command) return message.channel.send({ embeds: [commandNotFoundEmbed] });
+  if (!command) {
+    const commandNotFoundEmbed = new MessageEmbed()
+      .setTitle(
+        "Homie looks like that doesn't command exists."
+      )
+      .setDescription(
+        `Looking for a command? I got you homie, just do c+about to see all my commands and see what I do!`
+      )
+      .setFooter(
+        `${message.author.tag}`,
+        message.author.displayAvatarURL({ dynamic: true })
+      )
+      .setTimestamp();
+    
+    return message.channel.send({ embeds: [commandNotFoundEmbed] });
+  }
+  
   if (command) {
     try {
       await command.execute(client, message, args, Discord);
