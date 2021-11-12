7201963db96d422fa08a8e0bc00195b8f4a66acc
null <60427892+vierofernando@users.noreply.github.com>
2021-11-12 09:31:16 +0700

    refactor(images): did the same thing like bar.js

--- commands/balls/images.js
+++ commands/balls/images.js
@@ -5,8 +5,7 @@ module.exports = {
   name: "images",
   description: "we luv imags homie",
   async execute(client, message, args) {
-    const response = await axios.get("https://api.capybara.bar/");
-    const data = response.data;
-    message.channel.send(`${data.url}`);
+    const { data: { url } } = await axios.get("https://api.capybara.bar/");
+    message.channel.send(url);
   },
 };
